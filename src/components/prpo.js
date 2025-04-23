9d41bb18459552816cf2408e5b8cfa0103257e84
Satviky <126392608+Satviky@users.noreply.github.com>
2025-04-23 17:59:54 +0530

    policy updates

---
+++ src/components/prpo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const prpo = () => {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <h2>Information we collect</h2>
+            <p>Frankly speaking, we haven't added the backend to this website, so we are not collecting any information about you. Not even tracking the scores.
+            </p>
+            <h2>Is this website safe</h2>
+            <p>As long as you don't click on any of the suspicious advertisement, you are completely protected.
+                PS. We are not responsible for the ads you are watching on this site.
+            </p>
+            <h2>If we don't track your data how are we showing you relevant ads?</h2>
+            <p> We aren't. It's advertising agencies who bring the ads and plays the ads.</p>
+        </div>
+    )
+}
+
+export default prpo
