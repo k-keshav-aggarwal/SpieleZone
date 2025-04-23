43d91d4aa5ec5739ad174133d7fb99ad68bad005
Satviky <126392608+Satviky@users.noreply.github.com>
2025-04-23 18:02:23 +0530

    updated footer

--- src/components/Bottombar.js
+++ src/components/Bottombar.js
@@ -3,7 +3,14 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <footer className='bar'>
-            Made by Satviky || satvikgupta050404@gmail.com || Feel free to reach out
-        </footer>
+
+
+        <Link to="/about-us">About us</Link>
+
+        <Link to="/privacy-policy">Privacy Policy</Link>
+
+        <Link to="/reach-us">Contact Us</Link>
+
+    </footer>
     );
 }
