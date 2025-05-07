98874b3231fc4a44545a5040ea548432593c20e0
Satviky <126392608+Satviky@users.noreply.github.com>
2025-05-07 13:13:29 +0530

    fixing issues

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -3,24 +3,38 @@ import {Link} from 'react-router-dom'
 
 const aboutus = () => {
     return (
-        <div>
-            <h1>Who are we?</h1>
-            <p>Hi, I am a college student and I created this website while learning about React.js</p>
-            <p>When i was working on this site I didn't thought of developing it till here because i was just doing it as a part of my learning process but when I saw the response and attention this website was getting i decided to give it a shot.</p>
-            <p>You can figure it out that i am not good at being professional but if you reading this then please do come back to this site as much as possible. I need this... this is very much needed at this point.</p>
+<div>
+  <h1>About Spiele Zone</h1>
+  <p>Welcome to Spiele Zone! This website was created as a personal project to explore web development using React.js. What started as a learning endeavor has grown into a collection of fun and engaging web-based games.</p>
 
-            <h2>some more websites by the creator</h2>
-            <div className="lts">
-            <Link to="https://gsatvik.com">My resume</Link>
-            </div>
-            <div className="lts">
-            <Link to="https://project-rescue.onrender.com/">Personal Project #1</Link>
-            </div>
-            <div className="lts">
-            <Link to="https://pr-rescue.vercel.app/">Personal Project #1 [Demo Website]</Link>
-            </div>
-            
-        </div>
+  <p>The initial positive response to the website inspired me to continue developing it and share it with a wider audience. My aim is to provide a simple and enjoyable platform for casual gaming.</p>
+
+  <h2>About the Games</h2>
+  <p>Currently, Spiele Zone features the following games:</p>
+  <ul>
+    <li><strong>2048:</strong> A classic number-sliding puzzle game that challenges your strategic thinking.</li>
+    <li><strong>Snake Game:</strong> A timeless arcade game where you control a growing snake to eat and avoid obstacles. (Note: This game is currently being worked on and will be available soon.)</li>
+    <li><strong>Hangman:</strong> A word-guessing game that tests your vocabulary.</li>
+  </ul>
+
+  <h2>About the Creator</h2>
+  <p>I am a college student with a passion for learning and creating. This website is a way for me to combine my interest in technology and gaming. I am continuously working to improve Spiele Zone and potentially add more games in the future.</p>
+
+  <h2>More Projects by the Creator</h2>
+  <p>You can find more about my work and other projects here:</p>
+  <div className="lts">
+    <Link to="https://gsatvik.com">My Resume</Link>
+  </div>
+  <div className="lts">
+    <Link to="https://project-rescue.onrender.com/">Personal Project #1</Link>
+  </div>
+  <div className="lts">
+    <Link to="https://pr-rescue.vercel.app/">Personal Project #1 [Demo Website]</Link>
+  </div>
+
+  <h2>Contact</h2>
+  <p>If you have any questions or feedback about Spiele Zone, please feel free to reach out via the <a href="mailto:satvikgupta_it.aec@yahoo.com">Email[press here]</a>.</p>
+</div>
     )
 }
 
