af73e3b7abb2bb4e67c91a7e565c7e293619d5fc
Satviky <126392608+Satviky@users.noreply.github.com>
2025-05-07 12:55:37 +0530

    updating few things

--- src/components/prpo.js
+++ src/components/prpo.js
@@ -4,15 +4,29 @@ const prpo = () => {
     return (
         <div>
             <h1>Privacy Policy</h1>
+            <h2>April 26,2025</h2>
+
+
             <h2>Information we collect</h2>
-            <p>Frankly speaking, we haven't added the backend to this website, so we are not collecting any information about you. Not even tracking the scores.
+            <p>Currently, we do not directly collect any personally identifiable information from our users, such as names, email addresses, or scores within the games, as we do not have a backend system for user accounts or data storage.
             </p>
+
             <h2>Is this website safe</h2>
-            <p>As long as you don't click on any of the suspicious advertisement, you are completely protected.
-                PS. We are not responsible for the ads you are watching on this site.
-            </p>
+            <p>This website is completely safe as I am planning to present this website as my project to my college so I made sure that no unsafe or harmful material is used in the website. </p>
+
             <h2>If we don't track your data how are we showing you relevant ads?</h2>
-            <p> We aren't. It's advertising agencies who bring the ads and plays the ads.</p>
+            <p>
+                This Website uses third-party advertising services, such as Google AdSense, to display advertisements. These advertising partners may collect and use certain information about your visits to this Website and other websites to provide advertisements about goods and services of interest to you.
+                .</p>
+            <p>
+                The information that may be collected by these third-party advertising partners includes:
+
+                <b>Cookies:</b> Small text files stored on your browser.
+                <b>Device Information:</b> Information about the device you are using to access the Website, such as device type, operating system, and browser type.
+                <b>IP Address:</b> Your internet protocol address.
+                <b>Browsing Activity:</b> Information about your interaction with this Website and other websites you visit.
+                <b>Location Data:</b> General location information may be inferred from your IP address.
+            </p> 
         </div>
     )
 }
