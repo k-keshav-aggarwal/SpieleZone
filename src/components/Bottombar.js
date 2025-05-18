798e5e58c0e6a2da94828fc1403d2e14b9cd4fea
Satviky <126392608+Satviky@users.noreply.github.com>
2025-05-18 16:15:39 +0530

    updates

--- src/components/Bottombar.js
+++ src/components/Bottombar.js
@@ -4,11 +4,11 @@ export default function Navbar() {
     return (
         <footer className='bar'>
 
+        <Link title='About Shadowveil Studio' to="/about-us">About us</Link>
 
-        <Link to="/about-us">About us</Link>
+        <Link title='Read our privacy policy' to="/privacy-policy">Privacy Policy</Link>
 
-        <Link to="/privacy-policy">Privacy Policy</Link>
-
+        <Link title='Talk to Shadowveil StudioZ' to="/reach-us">Contact Us</Link>
 
     </footer>
     );
