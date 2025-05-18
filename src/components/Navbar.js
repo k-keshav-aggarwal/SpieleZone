bf91b3f8f514fe6eb3e1759e7c35b7318bbb7aea
Satviky <126392608+Satviky@users.noreply.github.com>
2025-05-18 16:17:20 +0530

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,10 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav className='bar'>
-            <div><Link to="/">Home</Link></div>
-            <div><Link to="/tzfe">2048</Link></div>
-            <div><Link to="/snake">Snake Game</Link></div>
-            <div><Link to="/hm">Hangman</Link></div>
-
+            <div><Link title='Link to Homepage' to="/">Home</Link></div>
+            <div><Link title='2048 game' to="/tzfe">2048</Link></div>
+            <div><Link title='Nostalgic Snake game' to="/snake">Snake Game</Link></div>
+            <div><Link title='Not so Classic Hangman' to="/hm">Hangman</Link></div>
         </nav>
     );
 }
