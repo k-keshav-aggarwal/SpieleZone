916644800e1c0c5696ec9b309f7b77e9704ff8e0
Satviky <126392608+Satviky@users.noreply.github.com>
2025-05-30 16:58:31 +0530

    CTA

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,6 +4,8 @@ import {Link} from 'react-router-dom'
 const contactus = () => {
     return (
         <div>
+            <h1>Contact Us</h1>
+            <p>We recommend you to contact us using the email provided below for quick response.</p>
             <h1>Our emails</h1>
             <li><a href="mailto:it10800222125@gmail.com" title="Reach Shadowveil Studioz via Email">Shadowveil StudioZ</a></li>
             <li><a href="mailto:satvikgupta050404@gmail.com" title="Reach Satvik Gupta via email">Satviky</a></li>
