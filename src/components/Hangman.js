eefe228504b6d635d990c4107b60883a9e13f637
Satviky <126392608+Satviky@users.noreply.github.com>
2025-06-04 13:23:02 +0530

    hangman updated

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -55,6 +55,23 @@ const Hangman = () => {
             document.head.removeChild(script);
         };
     }, []);
+    import { useEffect } from 'react';
+
+const AdWidget = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.setAttribute('data-cfasync', 'false');
+    script.src = '//pl26835984.profitableratecpm.com/d590a7eeede627e92ad37ac6c3ed8e9a/invoke.js';
+
+    document.getElementById('ad-container')?.appendChild(script);
+  }, []);
+
+  return <div id="ad-container"><div id="container-d590a7eeede627e92ad37ac6c3ed8e9a"></div></div>;
+};
+
+export default AdWidget;
+
 
     useEffect(() => {
         window.addEventListener('keydown', handleKeyPress);
@@ -89,6 +106,7 @@ const Hangman = () => {
                 </div>
                 {gameOver && <div className="game-over">Game Over! The word was "{word}".</div>}
                 {won && <div className="win">You win! The word was "{word}".</div>}
+                <AdWidget />
             </div>
         </>
     );
