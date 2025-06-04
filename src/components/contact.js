0e0b9c55cf3e70444a3b23f9dec615413968bb6a
Satviky <126392608+Satviky@users.noreply.github.com>
2025-06-04 10:32:35 +0530

    new commit

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,12 +8,14 @@ const contactus = () => {
             <p>We recommend you to contact us using the email provided below for quick response.</p>
             <h1>Our emails</h1>
             <li><a href="mailto:it10800222125@gmail.com" title="Reach Shadowveil Studioz via Email">Shadowveil StudioZ</a></li>
-            <li><a href="mailto:satvikgupta050404@gmail.com" title="Reach Satvik Gupta via email">Satviky</a></li>
+            <li><a href="mailto:satvikgupta_it.aec@yahoo.com" title="Reach Satvik Gupta via email">Satviky</a></li>
             <h2>Discord</h2>
             <li>
             <Link title='Talk to Shadowveil StudioZ' to="https://discord.com/users/1070314800615276555">Discord ID</Link>
-            
             </li>
+
+            <h3>DevNote:</h3>
+            <li>We accept New game request through Emails and discord</li>
         </div>
     )
 }   
