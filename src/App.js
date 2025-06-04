8819e22fd8411c440ee8b4cc15c020fcf0284edc
Satviky <126392608+Satviky@users.noreply.github.com>
2025-06-04 21:09:18 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Homepg from './components/homepg';
+
+import Home2 from './components/home2';
 import Tzfe from './components/tzfe';
 import SnakeGame from './components/SnakeGame';
 import Hangman from './components/Hangman';
@@ -11,6 +13,7 @@ import GCintro from './components/Ghostcode-intro'
 import GCplay from './components/GC'
 import Prpo from './components/prpo'
 import AU from './components/aboutus'
+import BounceGame from './components/bounce/bounce';
 
 import CU from './components/contact'
 
@@ -20,6 +23,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Homepg />} />
+          <Route path="/home" element={<Home2 />} />
           <Route path="/tzfe" element={<Tzfe />} />
           <Route path="/snake" element={<SnakeGame />} />
           <Route path="/hm" element={<Hangman />} />
@@ -28,6 +32,7 @@ function App() {
           <Route path="/reach-us" element={<CU />} />
           <Route path="/ghost-code" element={<GCintro />} />
           <Route path="/ghost-code/play" element={<GCplay />} />
+          <Route path='/bounce' element={<BounceGame/>}/>
         </Routes>
         <BBar />
       </Router>
---
+++ src/components/home2.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const home2 = () => {
+    return (
+        <div>
+            <h1>Welcome to spiele zone</h1>
+            <h2>home page</h2>
+
+            <ul className='game-tile-con'>
+                <li><Link to='/tzfe'><div className='game-tile t2048 zoom'>
+                <div className='gtitle'>2048</div>
+            </div></Link></li>
+                <li><Link to='/snake'><div className='game-tile tsnake zoom'>
+                <div className='gtitle'>Snake</div>
+            </div></Link></li>
+                <li><Link to='/hm'><div className='game-tile thangman zoom'>
+                <div className='gtitle'>Hangman</div>
+            </div></Link></li>
+                <li><Link to='/ghost-code/play'><div className='game-tile tgc zoom'>
+                <div className='gtitle'>Ghost Code</div>
+            </div></Link></li>
+                <li><Link to='/bounce'><div className='game-tile tbounce zoom'>
+                <div className='gtitle'>Bounce.exe</div>
+            </div></Link></li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default home2
