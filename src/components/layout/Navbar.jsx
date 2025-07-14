e98b17bdcb9d89ca54aff665aec7e2203e98aade
Satviky <126392608+Satviky@users.noreply.github.com>
2025-07-14 05:57:14 +0000

    -- html block added

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -23,15 +23,19 @@ export default function Navbar() {
         <Link title="New and Unique typing game" to="/ghost-code">Ghost Code</Link>
         <Link title="Simple Old School Memory Test" to="/memory" onClick={() => trackGameStart('hangman')}>Memory Puzzle</Link>
       </nav>
+
       <nav className={styles.pnavbar}>
-        <Link title="Link to Homepage" to="/">Home</Link>
-        <Link title="2048 game" to="/tzfe" onClick={() => trackGameStart('2048')}>2048</Link>
-        <Link title="Nostalgic Snake game" to="/Snake" onClick={() => trackGameStart('Snake')}>Snake Game</Link>
-        <Link title="Not so Classic Hangman" to="/HangMan" onClick={() => trackGameStart('hangman')}>Hangman</Link>
-        <Link title="New and Unique typing game" to="/ghost-code">Ghost Code</Link>
-        <Link title="Simple Old School Memory Test" to="/memory">Memory Puzzle</Link>
+        <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+        <div id="myDropdown" class="dropdown-content">
+          <Link title="Link to Homepage" to="/">Home</Link>
+          <Link title="2048 game" to="/tzfe" onClick={() => trackGameStart('2048')}>2048</Link>
+          <Link title="Nostalgic Snake game" to="/Snake" onClick={() => trackGameStart('Snake')}>Snake Game</Link>
+          <Link title="Not so Classic Hangman" to="/HangMan" onClick={() => trackGameStart('hangman')}>Hangman</Link>
+          <Link title="New and Unique typing game" to="/ghost-code">Ghost Code</Link>
+          <Link title="Simple Old School Memory Test" to="/memory">Memory Puzzle</Link>
+        </div>
       </nav>
-      
+
     </>
   );
 }
